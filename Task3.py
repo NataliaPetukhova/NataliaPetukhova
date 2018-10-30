6938ff457630d51132799ee32afbbb9a4c289f1c
NataliaPetukhova <43475014+NataliaPetukhova@users.noreply.github.com>
2018-10-30 17:33:27 +0300

    Update Task3.py
    Modified Task3

--- Task3.py
+++ Task3.py
@@ -1,42 +1,52 @@
+import itertools
+from functools import reduce
 
-#Problem1
+# Problem1
 def squares(a):
-	for i in a:
-		yield iter(i)**2
+    for i in a:
+        yield i**2
 
 
-#Problem2
+# Problem2
 def repeatntimes(elems, n):
-	it=itertools.tee(elems,n)
-	for i in it:
-		yield from i
+    it = itertools.tee(elems, n)
+    for i in it:
+        yield from i
 
 
-#Problem3
+# Problem3
 def evens(x):
-	if x%2:
-	   x=+1
-	while True:
-		x+=2
-		yield x
+    if x % 2:
+        x = +1
+    while True:
+        x += 2
+        yield x
 
 
-#Problem4
+# Problem4
 def digitsumdiv(p):
-	for i in itertolls.count(1):
-		if not sum(map(int,str(i)))%p:
-			yield i
+    for i in itertools.count(1):
+        if not sum(map(int, str(i))) % p:
+            yield i
 
-			
-#Problem5
+
+# Problem5
 def extractnumbers(s):
-	return filter(lambda x: x.isdigit(),s)
+    return filter(lambda x: x.isdigit(), s)
 
 
- 
-#Problem#6
+# Problem6
 def changecase(s):
-	return map(lambda x: x.swapcase() if x.isalpha() else x,s)
+        return map(lambda x: x.swapcase() if x.isalpha() else x, s)
 
 
->>> 
+# Problem7
+def productif(elem, cond):
+    return reduce(lambda x, y: x * y, map(lambda x: x[0] if x[1] else 1,
+zip(elem, cond)), 1)
+
+
+
+
+
+
