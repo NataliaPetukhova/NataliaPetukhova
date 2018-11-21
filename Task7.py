f35793a434b079a9b69561499fee344c639f6e1c
NataliaPetukhova <nvp.bioinfo@gmail.com>
2018-11-22 00:41:28 +0300

    Midterm fixed

--- Task7.py
+++ Task7.py
@@ -1,3 +1,7 @@
+import functools
+import itertools
+
+
 # Problem 1
 def valuesunion(*dicts):
     ans = set()
@@ -47,13 +51,29 @@ def popcount(n):
 
 
 # Problem 5
+import re
+
 def isIPv4(ip):
     return re.match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", ip) != None
 
 
 # Problem 6
-from functools import reduce
 
 def fibonacci(n):
-   return functools.reduce(lambda x,n:(x[0]+x[1],x[0]),[(1,1)]*(n-2))[0]
+    return functools.reduce(lambda x, n: [x[1], x[0] + x[1]], range(n), [0, 1])[0]
+
+
+# Problem 7
+def pascals():
+    prev = (1,)
+
+    for i in itertools.count(1):
+        act = []
+        act.append(1)
+        for k in range(len(prev) - 1):
+            act.append(prev[k] + prev[k + 1])
+        act.append(1)
+        yield tuple(prev)
+        prev = act
+
 
